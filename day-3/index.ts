9bd37f111c4afef1909ea41caeaf022e6e9bdf52
Zak <zakeria.ismail1@gmail.com>
2020-12-03 22:33:04 -0500

    add day 3 solution

---
+++ day-3/index.ts
@@ -0,0 +1,60 @@
+import { readFileSync } from 'fs'
+
+type Slope = {
+    x: number
+    y: number
+}
+
+const processInput = (): string[][] => {
+    const data = readFileSync('./input.txt', 'utf8');
+    const lines = data.split(/\r?\n/);
+    const map = []
+
+    for (const line of lines) {
+        map.push(line.split(''))
+    }
+
+    return map
+}
+
+const map = processInput()
+const MAP_WIDTH = map[0].length
+
+const traverseMap = (map: string[][], slope: Slope): number => {
+    let treeCount = 0
+    
+    const currentPosition = { x: 0, y: 0 }
+    while (currentPosition.y < map.length) {
+        const currentTerrain = map[currentPosition.y][currentPosition.x]
+    
+        if (currentTerrain === "#") {
+            treeCount ++
+        }
+    
+        currentPosition.y += slope.y
+        currentPosition.x += slope.x
+    
+        if (currentPosition.x >= MAP_WIDTH) {
+            currentPosition.x -= MAP_WIDTH
+        }
+    }
+    
+    return treeCount
+}
+
+const slopes: Slope[] = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+]
+
+let product = 1
+
+for (const slope of slopes) {
+    const treeCount = traverseMap(map, slope)
+    product *= treeCount
+}
+
+console.log(`The answer is ${product}.`)
